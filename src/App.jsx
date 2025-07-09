27c393085ff8505fd41f5360f08d054650d655df
Manu J <manunaidu.cj10@gmail.com>
2025-07-09 16:14:05 +0530

    Added Contact us,Quotation,Services Page

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,26 @@ import Header from "./Components/Header";
 import Navigation from "./Routes/Navigation";
 import Footer from "./Components/Footer";
 import ScrollToTop from "./Components/ScrollTop";
+import Lenis from '@studio-freight/lenis';
+import { useEffect } from "react";
 
 function App() {
+   useEffect(() => {
+    const lenis = new Lenis({
+      duration: 1.2,
+      smooth: true,
+    });
+
+    const raf = (time) => {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    };
+
+    requestAnimationFrame(raf);
+
+    return () => lenis.destroy();
+  }, []);
+
   return (
     <div className="relative">
       <Background />
