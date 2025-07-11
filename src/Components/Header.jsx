081ce9b257a94aba415b1e818a1f1aedf613a3f2
Manu J <manunaidu.cj10@gmail.com>
2025-07-11 14:57:52 +0530

    Website Design is Completed and Email is Also Working

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -15,10 +15,12 @@ function Header() {
                       backdrop-blur-lg bg-white/10 border border-cyan-500/30 rounded-full 
                       px-4 py-2 sm:px-5 sm:py-3 flex items-center justify-between shadow-xl max-w-full">
         <Link to="/">
-          <div className="text-white text-sm sm:text-base md:text-lg font-bold tracking-wide">
-            ChanReMedSoft
+          <div className="text-white text-sm sm:text-base md:text-lg font-bold tracking-wide flex items-center ">
+            <img src="/logo.png" alt="logo" className="h-12 w-auto" />
+            <span>ChanReMedSoft</span>
           </div>
         </Link>
+
 
         <div className="hidden md:flex gap-6 text-sm  sm:text-base items-center">
           <Link to="/" className="text-white/80 hover:text-cyan-300 transition">Home</Link>
